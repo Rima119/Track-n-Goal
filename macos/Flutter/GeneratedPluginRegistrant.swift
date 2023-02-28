5590a8bd82d3c0548fc337b5c53c62b7683d98be
Rima119 <89083884+Rima119@users.noreply.github.com>
2023-02-28 20:40:58 +0100

    add

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_auth
-import firebase_core
-import flutter_secure_storage_macos
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
